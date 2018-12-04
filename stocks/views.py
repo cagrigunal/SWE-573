83b288029200bc63a9eca819215109d581366a6a
Cagri Gunal <cagri.gunal@gmail.com>
2018-12-04 16:50:23 +0300

    Update views.py

--- stocks/views.py
+++ stocks/views.py
@@ -27,10 +27,10 @@ from django.utils import timezone
 
 
 # Your credentials go here
-consumer_key = "XXdVWTaTgOkR0AHaE1SOG1fWa"
-consumer_secret = "MH7ZWKMoiSjnw0kZweLpKzTlSXjpVE27aZfuJsI8twC3Jm7VOH"
-access_token = "28985708-GTz8sReJqi039EaadKljnbxQYu6gPk4n810bCjFDa"
-access_token_secret = "J4cOQ4KZwOtyN9LUIBz2h69F56fGnWWyNV3OXLaRt0MuS"
+consumer_key = "a"
+consumer_secret = "b"
+access_token = "c"
+access_token_secret = "d"
 
 """
 The code for our listener class above goes here!
